7281ad943eacd1e959341bdff17219422ce04c20
jessepincay <pincayjesse@gmail.com>
2026-09-23 22:43:32 +0000

    Revert "DSPVolumeSynchronizer: Fix boot receiver and service lifecycle"
    This reverts commit dbe06707041e2eb7aa7b5eb85c733e31e62e32ac.

--- packages/DSPVolumeSynchronizer/src/org/lineageos/dspvolume/xiaomi/BootReceiver.java
+++ packages/DSPVolumeSynchronizer/src/org/lineageos/dspvolume/xiaomi/BootReceiver.java
@@ -4,17 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (context == null || intent == null) {
+        if (context == null) {
             return;
         }
-
-        if (!Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            return;
-        }
-
         context.startService(new Intent(context, VolumeListenerService.class));
     }
 }
